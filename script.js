3550e35fc7fd9dea77496f5e6128ba6d9c6d85c4
Moamal-2000 <moamalalaapro1@gmail.com>
2024-12-11 15:53:47 +0300

    feat: render card play dynamically

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 "use strict";
 
+const playCardsContainer = document.querySelector(".game-cards");
 
+const cardsColors = ["red", "yellow", "green", "blue"];
+const numberOfCards = 9;
+
+for (let colorIndex = 0; colorIndex < cardsColors.length; colorIndex++) {
+  const color = cardsColors[colorIndex];
+  renderCardsByColor(playCardsContainer, color);
+}
+
+function renderCardsByColor(elementParent, color) {
+  for (let number = 1; number <= numberOfCards; number++) {
+    const lineClass = number === 9 || number === 6 ? "line" : "";
+
+    elementParent.innerHTML += `
+    <div class="card-body ${color}">
+      <div class="center-circle">
+        <span class="${lineClass}" data-number=${number}>${number}</span>
+      </div>
+    </div>`;
+  }
+}
